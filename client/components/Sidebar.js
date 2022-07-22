6713429fc8cdd415f196b55975464e362fe23f80
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-22 20:59:36 +0100

    Sidebar and basic navigation

---
+++ client/components/Sidebar.js
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import { VscTwitter } from 'react-icons/vsc'
+import SidebarOption from './SidebarOption'
+import { RiHome7Line, RiHome7Fill, RiFileList2Fill } from 'react-icons/ri'
+import { BiHash } from 'react-icons/bi'
+import { FiBell, FiMoreHorizontal } from 'react-icons/fi'
+import { HiOutlineMail, HiMail } from 'react-icons/hi'
+import { FaRegListAlt, FaHashtag, FaBell } from 'react-icons/fa'
+import { BsBookmarkFill, BsBookmark, BsPersonFill, BsPerson } from 'react-icons/bs'
+import { CgMoreO } from 'react-icons/cg'
+
+
+const style = {
+    wrapper: `flex-[0.7] px-8 flex flex-col`,
+    twitterIconContainer: `text-3xl m-4`,
+    tweetBtn: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] mt-[20px] cursor-pointer`,
+    navContainer: `flex-1`,
+    profileBtn: `flex items-center mb-6 cursor-pointer hover:bg-[#333c45] rounded-[100px] p-2`,
+    profileLeft: `flex item-center justify-center mr-4`,
+    profileImg: `height-12 w-12 rounded-full`,
+    profileRight: `flex-1 flex`,
+    details: `flex-1`,
+    name: `text-lg`,  
+    handle: `text-[#8899a6]`,
+    moreContainer: `flex items-center mr-2`,
+
+}
+
+const Sidebar = ({ initialSelected = 'House' }) => {
+  const [selected, setSelected] = useState(initialSelected)
+  return (
+    <div className={style.wrapper}>
+        <div className={style.twitterIconContainer}>
+            <VscTwitter />
+        </div>
+        <div className={style.navContainer}>
+            <SidebarOption 
+              Icon={selected == 'Home' ? RiHome7Fill : RiHome7Line}
+              text="Home"
+              isActive={Boolean(selected == 'Home')}
+              setSelected={setSelected}
+              redirect={'/'}
+            />
+            <SidebarOption 
+              Icon={selected == 'Explore' ? FaHashtag : BiHash}
+              text="Explore"
+              isActive={Boolean(selected == 'Explore')}
+              setSelected={setSelected}
+            />
+            <SidebarOption 
+              Icon={selected == 'Notifications' ? FaBell : FiBell}
+              text="Notifications"
+              isActive={Boolean(selected == 'Notifications')}
+              setSelected={setSelected}
+            />
+            <SidebarOption 
+              Icon={selected == 'Messages' ? HiMail : HiOutlineMail}
+              text="Messages"
+              isActive={Boolean(selected == 'Messages')}
+              setSelected={setSelected}
+            />
+            <SidebarOption 
+              Icon={selected == 'Bookmarks' ? BsBookmarkFill : BsBookmark}
+              text="Bookmarks"
+              isActive={Boolean(selected == 'Bookmarks')}
+              setSelected={setSelected}
+            />
+            <SidebarOption 
+              Icon={selected == 'Lists' ? RiFileList2Fill : FaRegListAlt}
+              text="Lists"
+              isActive={Boolean(selected == 'Lists')}
+              setSelected={setSelected}
+            />
+            <SidebarOption 
+              Icon={selected == 'Profile' ? BsPersonFill : BsPerson}
+              text="Profile"
+              isActive={Boolean(selected == 'Profile')}
+              setSelected={setSelected}
+              redirect={'/profile'}
+            />
+            <SidebarOption Icon={CgMoreO}
+              text="More"
+              isActive={Boolean(selected == 'More')}
+              setSelected={setSelected}
+            />
+
+            <div className={style.tweetBtn}>Mint</div>
+        </div>
+
+        <div className={style.profileBtn}>
+          <div className={style.profileLeft}></div>
+          <div className={style.profileRight}>
+            <div className={style.details}>
+              <div className={style.name}>code_shinobi</div>
+              <div className={style.handle}>@0x86...5f2df</div>
+            </div>
+            <div className={style.moreContainer}>
+              <FiMoreHorizontal />
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
